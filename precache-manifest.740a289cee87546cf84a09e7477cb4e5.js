3d0dc8634be0e93e6fdc37c5d9a8db58a6240589
ferozpuri <ferozpuri@users.noreply.github.com>
2021-10-17 09:29:29 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.740a289cee87546cf84a09e7477cb4e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc07718588d7f716bf569bd213b70349",
+    "url": "https://ferozpuri.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "ae4e8a13c7593ca46d5c",
+    "url": "https://ferozpuri.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "ae93e7bcab9d1b7528f3",
+    "url": "https://ferozpuri.github.io/task-management-frontend/static/js/2.ccbe58d7.chunk.js"
+  },
+  {
+    "revision": "ae4e8a13c7593ca46d5c",
+    "url": "https://ferozpuri.github.io/task-management-frontend/static/js/main.b0418328.chunk.js"
+  },
+  {
+    "revision": "08229bb8bc268d8e0185",
+    "url": "https://ferozpuri.github.io/task-management-frontend/static/js/runtime~main.3f7c382a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://arielweinberger.github.io/task-management-frontend/precache-manifest.0a88baa7b4ebb92ea89c64cd3e3f4832.js"
+  "https://ferozpuri.github.io/task-management-frontend/precache-manifest.740a289cee87546cf84a09e7477cb4e5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://arielweinberger.github.io/task-management-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://ferozpuri.github.io/task-management-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
